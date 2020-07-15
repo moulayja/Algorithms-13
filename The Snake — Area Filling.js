98a9c1df4352c7088c20b75a984ec926116072cb
Jamal <moulayjam@gmail.com>
2020-07-15 11:43:16 -0400

    Renaming

---
+++ The Snake — Area Filling.js
@@ -0,0 +1,20 @@
+
+/*
+Assuming that you know the popular game "Snake", you have an area of x*x,
+the snake length is 1 (only the head) and is positioned in the top left corner of the area at
+the beginning which means if your area is 7*7
+
+Knowing that each time the snake eats the food his length will be 2 times longer 
+(so if the length of the snake is 4, after eating it will be 8),
+you have to create a function that takes a number as an argument and returns how many times
+the snake needs to eat to fill the entire area.
+
+Examples
+snakefill(3) ➞ 3
+
+snakefill(6) ➞ 5
+
+snakefill(24) ➞ 9
+
+*/
+
